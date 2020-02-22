22579623cabc673b7a75030a5bb9967b7cc62312
ChistovPavel <pavlik.9947@gmail.com>
2020-02-22 23:14:07 +0300

    second commit

---
+++ src/com/chistov/pavel/Main.java
@@ -0,0 +1,43 @@
+package com.chistov.pavel;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        OwnStack list_1 = new OwnStack(Arrays.asList(1,2,3,4,5,6,7));
+        try {
+            OwnStack list_2 = new OwnStack(list_1.clone(), 2, Arrays.asList(7,8,9,10,11,56,87,7,7,7,7,7,0));
+
+            System.out.println("first stack: " + list_1.clone().Pop_all());
+            System.out.println("second stack: " + list_2.clone().Pop_all());
+            System.out.println("Found value: " + FindCommonNode(list_1.clone(), list_2.clone()));
+        }
+        catch (CloneNotSupportedException ex) {
+            System.out.println("Cloneable not implemented");
+        }
+    }
+
+    static public Integer FindCommonNode(OwnStack list_1, OwnStack list_2)
+    {
+        while (list_1.Length() != list_2.Length())
+        {
+            if (list_1.Length() > list_2.Length())list_1.Pop();
+            else list_2.Pop();
+        }
+
+        while (!list_1.HeadIsNull() && !list_2.HeadIsNull())
+        {
+            if (list_1.show() == list_2.show())
+            {
+                return list_1.show();
+            }
+            list_1.Pop();
+            list_2.Pop();
+        }
+
+        return 0;
+    }
+}
---
+++ src/com/chistov/pavel/OwnStack.java
@@ -0,0 +1,104 @@
+package com.chistov.pavel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OwnStack implements Cloneable{
+
+    private ForwardList head;
+    private Integer length;
+
+    public OwnStack(List<Integer> array)
+    {
+        this.head = new ForwardList();
+        this.head.setData(array.get(0));
+        this.length = 1;
+
+        for (int i = 1; i < array.size(); i++)
+        {
+            this.Push(array.get(i));
+        }
+    }
+
+    public OwnStack(OwnStack stack, Integer commonNode, List<Integer> array)
+    {
+        int len = stack.Length() - commonNode;
+        for (int i = 0; i < len; i++)
+        {
+            stack.Pop();
+        }
+        this.head = stack.head;
+        this.length = stack.Length();
+
+        for (int i = 0; i < array.size(); i++)
+        {
+            this.Push(array.get(i));
+        }
+    }
+
+    public void Push(Integer value){
+        if (head == null)
+        {
+            head = new ForwardList();
+            head.setData(value);
+            this.length = 1;
+        }
+        else
+        {
+            ForwardList new_head = new ForwardList();
+            new_head.setData(value);
+            new_head.setNextElement(head);
+            head = new_head;
+            this.length++;
+        }
+    }
+    public Integer Pop(){
+        if (head == null)
+        {
+            this.length = 0;
+            return null;
+        }
+        else
+        {
+            Integer ret = head.getData();
+            head = head.next();
+            this.length--;
+            return ret;
+        }
+    }
+
+    public Integer show()
+    {
+        return head.getData();
+    }
+
+    public List<Integer> Pop_all(){
+
+        List<Integer> ret = new ArrayList<>();
+
+        while (head != null)
+        {
+            ret.add(head.getData());
+            head = head.next();
+        }
+
+        this.length = 0;
+
+        return ret;
+    }
+
+    public boolean HeadIsNull()
+    {
+        return (head == null);
+    }
+
+    public OwnStack clone() throws CloneNotSupportedException{
+        OwnStack new_list = (OwnStack) super.clone();
+        return new_list;
+    }
+
+    public Integer Length()
+    {
+        return this.length;
+    }
+}
